92220b33301bbd299669236ce3a1b32d1960bb15
leekyongsoo <82308702+leekyongsoo@users.noreply.github.com>
2021-06-20 21:13:04 +0900

    Delete infrared.model.js

--- infrared.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-var InfraredSchema = mongoose.Schema({   //스키마(Schema) 설정
-  infraredId: {type: Number, require:true},
-  state: { type: String, require: true},
-});
-
-module.exports = mongoose.model('Infrared', InfraredSchema); //소문자화 후 복수형으로 바꾸어 infrareds 컬렉션이 됨
