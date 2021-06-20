619a6448421e9635dc792423c7d5feddad875dc3
leekyongsoo <82308702+leekyongsoo@users.noreply.github.com>
2021-06-20 21:13:10 +0900

    Delete infrared.router.js

--- infrared.router.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-  const infrareds = require('./infrared.controller.js');
-  app.get('/infrareds', infrareds.findAll);
-  app.get('/infrareds/:infraredId', infrareds.findOne);
-  app.post('/infrareds',infrareds.create);
-  app.put('/infrareds/:infraredId', infrareds.update);
-  app.delete('/infrareds/:infraredId', infrareds.delete);
-}
